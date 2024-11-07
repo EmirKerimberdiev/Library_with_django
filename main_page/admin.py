520f7a0750457127c6e1d25a3b028be4aebebdaa
EmirKerimberdiev <keremberdievemir43@gmail.com>
2024-11-07 06:21:57 +0600

    add site for this code

--- main_page/admin.py
+++ main_page/admin.py
@@ -1,15 +1,6 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Books)
 
 
-def about_me(request):
-    if request.method == 'GET':
-        return HttpResponse("👨‍💻Hello! My name is Emir and I am a would-be a Backend Developer👨‍💻.")
-
-
-def about_my_pets(request):
-    if request.method == 'GET':
-        return HttpResponse("<img src='https://myfavoritepets.ru/wp-content/uploads/news373_1.jpg'>- This is my parrot, Grigoriy", )
-
-
-def system_time(request):
-    if request.method == 'GET':
-        return HttpResponse(datetime.datetime.now())
--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    GENRE_CHOICE = (
+        ('Приключения', 'Приключения'),
+        ('Книга рецептов', 'Книга рецептов'),
+        ('Фентези', 'Фентези')
+    )
+    image = models.ImageField(upload_to='books/', verbose_name='загрузите картинку')
+    title = models.CharField(max_length=100, verbose_name='Напишите название книги')
+    description = models.TextField(verbose_name='Напишите описание книги', default='Lorem Ipsum', null=True)
+    price = models.FloatField(verbose_name='Укажите цену книги')
+    date_published = models.DateField(verbose_name='Укажите дату публикации книги', null=True)
+    genre = models.CharField(max_length=100, choices=GENRE_CHOICE, default='Приключения',verbose_name='Укажите жанр книги')
+    pages = models.CharField(max_length=50, verbose_name='Укажите количество страниц в книге')
+    autor = models.CharField(max_length=30, verbose_name='Укажите автора книги')
+    audio_book = models.URLField(verbose_name='Укажите ссылку на аудио-книгу')
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return f'{self.title}-{self.price}$'
